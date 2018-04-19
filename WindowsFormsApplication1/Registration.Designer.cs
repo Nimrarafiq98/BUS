09532574bdd94db0c03b9ee7aaa41e5cd268a440
Nimrarafiq98 <nimrarafiq98@gmail.com>
2018-04-19 10:42:49 +0500

    login

---
+++ WindowsFormsApplication1/Registration.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.cmdRegistration = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lnklblLogin = new System.Windows.Forms.LinkLabel();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(157, 114);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(157, 64);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 10;
+            // 
+            // cmdRegistration
+            // 
+            this.cmdRegistration.Location = new System.Drawing.Point(182, 176);
+            this.cmdRegistration.Name = "cmdRegistration";
+            this.cmdRegistration.Size = new System.Drawing.Size(75, 23);
+            this.cmdRegistration.TabIndex = 9;
+            this.cmdRegistration.Text = "Login";
+            this.cmdRegistration.UseVisualStyleBackColor = true;
+            this.cmdRegistration.Click += new System.EventHandler(this.cmdRegistration_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(27, 114);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 8;
+            this.lblPassword.Text = "Password";
+            // 
+            // lnklblLogin
+            // 
+            this.lnklblLogin.AutoSize = true;
+            this.lnklblLogin.Location = new System.Drawing.Point(27, 181);
+            this.lnklblLogin.Name = "lnklblLogin";
+            this.lnklblLogin.Size = new System.Drawing.Size(58, 13);
+            this.lnklblLogin.TabIndex = 7;
+            this.lnklblLogin.TabStop = true;
+            this.lnklblLogin.Text = "registration";
+            this.lnklblLogin.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnklblLogin_LinkClicked);
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(27, 72);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 6;
+            this.lblUsername.Text = "Username";
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.cmdRegistration);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lnklblLogin);
+            this.Controls.Add(this.lblUsername);
+            this.Name = "Registration";
+            this.Text = "LOGIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button cmdRegistration;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.LinkLabel lnklblLogin;
+        private System.Windows.Forms.Label lblUsername;
+    }
+}
