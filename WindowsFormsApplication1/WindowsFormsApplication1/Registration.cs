4272492254c1375c3338be5a9611451a0d1af684
2016CS371 <ayeshaidrees482@gmal.com>
2018-04-19 09:56:23 +0500

    login

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Registration.cs
@@ -0,0 +1,34 @@
+﻿                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdRegistration_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lnklblLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form1 registration = new Form1();
+            this.Hide();
+            registration.Show();
+        }
+
+        }
+    }
+
