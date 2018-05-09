7888a5074b35f9a6a2030893464ed38b80ae566c
SumeyaAnjum <sumeyaanjum32@gmail.com>
2018-05-09 06:41:08 -0700

    calculate distance (complete integration)

--- Routes/Routes/Search.cs
+++ Routes/Routes/Search.cs
@@ -23,31 +23,6 @@ namespace Routes
             bool searchpick;
             bool searchpickpassed;
             server.searchpick(txtPickup.Text, out searchpick, out searchpickpassed);
-
-            bool searchdown;
-            bool searchdownpassed;
-            server.searchdown(txtDropdown.Text, txtPickup.Text, out searchdown, out searchdownpassed);
-            if (searchdown)
-            {
-                MessageBox.Show("route founded");
-                Server.Service1 my = new Server.Service1();
-                int distance;
-                bool distancepassed;
-                my.distance(txtPickup.Text, txtDropdown.Text, out distance, out distancepassed);
-                if (distancepassed)
-                {
-
-                    Server.Service1 myserver = new Server.Service1();
-                    BindingSource bs = new BindingSource();
-                    bs.DataSource = myserver.getsearch();
-                    dataGridView1.DataSource = bs;
-
-                }
-            } //jhjguxgs\ bind
-            else
-            {
-                MessageBox.Show("route not founded");
-            }
          
             bool searchdown;
             bool searchdownpassed;
